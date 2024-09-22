5116faf47b71ca4f0592b66bcbfa41b842d5b031
JuniorMCF <juniorcastillon6@gmail.com>
2024-09-22 12:29:05 -0500

    modificacion de registro en base a la tabla usuario y el login

--- app/Http/Controllers/Api/V1/Login/LoginUserController.php
+++ app/Http/Controllers/Api/V1/Login/LoginUserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\V1\Login;
 
 use App\Http\Controllers\ApiController;
-use App\Models\CustomUser;
 use App\Models\Socio;
 use App\Models\CobranzaAlta;
 use App\Models\Usuario;
@@ -18,67 +17,59 @@ class LoginUserController extends ApiController
     public function signIn(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'login' => 'required|string', //dni
-            'password' => 'required|string', //codigo - 1er apellido
+            'login' => 'required|string', // dni o ruc
+            'password' => 'required|string', // clave
         ]);
 
         if ($validator->fails()) {
             return $this->errorResponse($validator->errors()->all(), 422);
         }
-        // Verificar si el usuario existe por dni o ruc
-        $user = Socio::where('dni', $request->login)
-            ->orWhere('ruc', $request->login)
-            ->first();
 
+        // Verificar si el usuario existe por dni o ruc en el campo dni
+        $user = Usuario::where('dni', $request->login)->first();
+
+        // Si no se encuentra el usuario
         if (!$user) {
             return $this->errorResponse("Credenciales no válidas", 404);
         }
-        /**
-         * si el usuario existe tenemos que verificar si cumple con los requisitos para usar la aplicacion
-         * es decir que escobranzaaplic  o  esreportegerencial  este en 1
-         */
 
-        if (!Usuario::where('idsocio', optional($user)->idsocio)
-            ->when(function ($query) {
-                return $query->where('escobranzaaplic', 1)
+        // Verificar si el usuario tiene acceso permitido a la aplicación
+        $usuarioPermisos = Usuario::where('idsocio', $user->idsocio)
+            ->where(function ($query) {
+                $query->where('escobranzaaplic', 1)
                     ->orWhere('esreportegerencial', 1);
-            })
-            ->first()) {
+            })->first();
 
-            return $this->errorResponse("Usuario no autorizado", 404);
+        // Si no tiene permisos, se le permitirá el acceso pero se ajustarán los permisos a 0
+        if (!$usuarioPermisos) {
+            Usuario::where('idsocio', $user->idsocio)->update([
+                'escobranzaaplic' => 0,
+                'esreportegerencial' => 0,
+            ]);
         }
 
-
-        if (!CobranzaAlta::where("dni", $request->login)->first()) {
+        // Verificar si el usuario ha completado su registro
+        if (!CobranzaAlta::where("dni", $request->login)->exists()) {
             return $this->errorResponse("El usuario no ha completado su registro", 403);
         }
 
-        if (!CobranzaAlta::where("dni", $request->login)->where('status',1)->first()) {
+        // Verificar si el usuario ha sido dado de baja
+        if (!CobranzaAlta::where("dni", $request->login)->where('status', 1)->exists()) {
             return $this->errorResponse("El usuario ha sido dado de baja", 404);
         }
 
-        /**si existe el usuario verificamos que coincida con la clave*/
+        // Verificar la clave
         $user = CobranzaAlta::where("dni", $request->login)->first();
-
-
-
         if (Hash::check($request->password, $user->pswd)) {
-
+            // Traer los datos de socio y algunos campos específicos del usuario
             $data = Socio::leftJoin('usuario', 'usuario.idsocio', '=', 'socio.idsocio')
-                ->where(function ($query) use ($user) {
-                    $query->where('socio.idsocio', $user->idsocio);
-                })
-                ->select('socio.*', 'usuario.escobranzaaplic', 'usuario.esreportegerencial','usuario.nombre','usuario.nombrelargo')
+                ->where('socio.idsocio', $user->idsocio)
+                ->select('socio.*', 'usuario.escobranzaaplic', 'usuario.esreportegerencial', 'usuario.nombre', 'usuario.nombrelargo')
                 ->first();
-
-            if (!isset($data->estado)) {
-                $data->setAttribute('estado', 'A');
-            }
 
             return $this->successResponse($data);
         } else {
             return $this->errorResponse("Credenciales no válidas", 404);
         }
     }
-
 }
--- app/Http/Controllers/Api/V1/Register/RegisterController.php
+++ app/Http/Controllers/Api/V1/Register/RegisterController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers\Api\V1\Register;
 use App\Http\Controllers\ApiController;
 
 use App\Mail\Registro;
-use App\Models\Socio;
 use App\Models\CobranzaAlta;
-
+use App\Models\Usuario;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
@@ -27,21 +25,16 @@ class RegisterController extends ApiController
             return $this->errorResponse($validator->errors()->all(), 422);
         }
 
-        //verificamos que exista el usuario
-        if (!Socio::where(function ($query) use ($request) {
-            $query->where('dni', $request->dni)
-                ->orWhere('ruc', $request->dni); // Buscar coincidencias en dni o ruc
-        })->where(function ($query) {
-            $query->where('idtipopersona', 2);//trabajador
-
-        })->first()) {
+        // Verificar si el usuario existe solo por el campo dni en la tabla Usuario
+        if (!Usuario::where('dni', $request->dni)->first()) {
             return $this->errorResponse("Usuario no encontrado", 201);
         } else if (!CobranzaAlta::where("dni", $request->dni)->first()) {
-            return $this->errorResponse("Aún no válida su correo electrónico", 202);
+            return $this->errorResponse("Aún no valida su correo electrónico", 202);
         } else {
             return $this->successResponse(CobranzaAlta::where("dni", $request->dni)->first());
         }
     }
+
     public function confirmEmail(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -52,65 +45,51 @@ class RegisterController extends ApiController
             return $this->errorResponse($validator->errors()->all(), 422);
         }
 
-        // Buscar usuario por dni o ruc en la tabla Socio
-        $socio = Socio::where(function ($query) use ($request) {
-            $query->where('dni', $request->dni)
-                ->orWhere('ruc', $request->dni); // Buscar coincidencias en dni o ruc
-        })->where(function ($query) {
-            $query->where('idtipopersona', 2); // trabajador
-        })->first();
+        // Buscar usuario por dni en la tabla Usuario
+        $usuario = Usuario::where('dni', $request->dni)->first();
 
-        if (!$socio) {
+        if (!$usuario) {
             return $this->errorResponse("Usuario no encontrado", 201);
         } else {
-            /** Verificar si este correo está siendo usado por otro usuario */
+            // Verificar si este correo ya está siendo usado por otro usuario
             $existingUser = CobranzaAlta::where('email', $request->email)
-                ->where('idsocio', '!=', $socio->idsocio)
+                ->where('idsocio', '!=', $usuario->idsocio)
                 ->first();
 
             if ($existingUser) {
                 return $this->errorResponse("El correo electrónico ya está registrado para otro usuario", 202);
             } else {
-                return $this->successResponse($socio);
+                return $this->successResponse($usuario);
             }
         }
     }
+
     public function confirmBorn(Request $request)
     {
-
         $validator = Validator::make($request->all(), [
             'idsocio' => 'required',
             'dni' => 'required',
             'phone_number' => 'required',
         ]);
+
         if ($validator->fails()) {
             return $this->errorResponse($validator->errors()->all(), 422);
         }
 
-        /**buscaremos el socio por el dni,
-         * luego verificamos que coincida con el id de socio y la fecha de cumpleaños enviado
-         * si alguno falla devolvemos un error 201
-         *
-         */
-        $socio = Socio::where(function ($query) use ($request) {
-            $query->where('dni', $request->dni)
-                ->orWhere('ruc', $request->dni); // Buscar coincidencias en dni o ruc
-        })->where(function ($query) {
-            $query->where('idtipopersona', 2); // trabajador
-        })->first();
+        // Buscar en la tabla Usuario por dni
+        $usuario = Usuario::where('dni', $request->dni)->first();
 
-        // \Log::debug(print_r($socio,true));
-        // \Log::debug(print_r($request->all(),true));
-        if (!$socio) {
+        if (!$usuario) {
             return $this->errorResponse("Usuario no encontrado", 201);
         } else {
-            if ($socio->idsocio == $request->idsocio && ($request->phone_number == $socio->telmovil || $request->phone_number == $socio->telfijo)) {
-                return $this->successResponse($socio);
+            if ($usuario->idsocio == $request->idsocio && ($request->phone_number == $usuario->telmovil || $request->phone_number == $usuario->telfijo)) {
+                return $this->successResponse($usuario);
             } else {
                 return $this->errorResponse("Credenciales no válidas", 202);
             }
         }
     }
+
     public function confirmPassword(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -124,52 +103,39 @@ class RegisterController extends ApiController
         if ($validator->fails()) {
             return $this->errorResponse($validator->errors()->all(), 422);
         }
-        /**procedemos a almacenar estos datos en un nuevo registro */
 
-        $socio = Socio::where(function ($query) use ($request) {
-            $query->where('dni', $request->dni)
-                ->orWhere('ruc', $request->dni); // Buscar coincidencias en dni o ruc
-        })->where(function ($query) {
-            $query->where('idtipopersona', 2); // trabajador
+        // Buscar en la tabla Usuario por dni
+        $usuario = Usuario::where('dni', $request->dni)->first();
 
-        })->first();
-
-        if (!$socio) {
-            return $this->errorResponse("Usuario no encontrado", 201); // el usuario no se encuentra registrado
+        if (!$usuario) {
+            return $this->errorResponse("Usuario no encontrado", 201);
         } else {
-
-            if (!CobranzaAlta::where('idsocio', $socio->idsocio)->first()) {
-
-                $socio_alta = CobranzaAlta::create([
-                    'idsocio' => $socio->idsocio,
-                    'nom' => $socio->nom,
-                    'ap' => $socio->ap,
-                    'am' => $socio->am,
-                    'fec_nac' => $socio->fnac,
+            // Verificar si el usuario ya tiene registro en CobranzaAlta
+            if (!CobranzaAlta::where('idsocio', $usuario->idsocio)->first()) {
+                $usuario_alta = CobranzaAlta::create([
+                    'idsocio' => $usuario->idsocio,
+                    'nom' => $usuario->nombre,
+                    'ap' => $usuario->ap,
+                    'am' => $usuario->am,
+                    'fec_nac' => $usuario->fnac,
                     'dni' => $request->dni,
                     'email' => $request->email,
                     'secret_question' => $request->question,
                     'secret_answer' => $request->answer,
                     'pswd' => Hash::make($request->password),
-                    'status'=>1
+                    'status' => 1
                 ]);
 
-                /**aqui debemos enviar correo electronico de bienvenida */
-
+                // Enviar correo electrónico de bienvenida
                 try {
-                    Mail::to($request->email)->send(new Registro($socio_alta));
+                    Mail::to($request->email)->send(new Registro($usuario_alta));
 
-                    // Actualizar el correo del socio en la base de datos original dentro de una transacción
-                    // DB::transaction(function () use ($socio, $request) {
-                    //     $socio->email = $request->email;
-                    //     $socio->save();
-                    // });
+                    // Opcionalmente, actualiza el correo del usuario en la base de datos
                 } catch (\Exception $e) {
                     \Illuminate\Support\Facades\Log::error('Error al enviar correo: ' . $e->getMessage());
-                    $this->successResponse($socio_alta);
                 }
 
-                return $this->successResponse($socio_alta);
+                return $this->successResponse($usuario_alta);
             } else {
                 return $this->errorResponse("Este usuario ya se encuentra registrado", 202);
             }
