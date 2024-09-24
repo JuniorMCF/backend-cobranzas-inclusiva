3f0a6bd0827e2751660c831ed33c6ce8e749e4a5
JuniorMCF <juniorcastillon6@gmail.com>
2024-09-23 20:29:01 -0500

    eliminar cobranza modificado para no tocar el total

--- app/Http/Controllers/Api/V1/App/AppController.php
+++ app/Http/Controllers/Api/V1/App/AppController.php
@@ -18,6 +18,7 @@ use App\Models\Usuario;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class AppController extends ApiController
